7e032e854d21eac94736486c61b442537005b9c8
epizod <>
2026-04-16 20:08:28 +0300

    fix

--- convert.js
+++ convert.js
@@ -1,39 +1,29 @@
 const fs = require("fs");
 const { execSync } = require("child_process");
 
-const input = "ipwhitelist.txt";
-const jsonFile = "ipwhitelist.json";
-const output = "ipwhitelist.srs";
+const url =
+  "https://github.com/hxehex/russia-mobile-internet-whitelist/raw/refs/heads/main/cidrwhitelist.txt";
 
-if (!fs.existsSync(input)) {
-    console.error("ips.txt not found");
-    process.exit(1);
-}
+(async () => {
+  const res = await fetch(url);
+  const text = await res.text();
 
-// читаем IP
-const lines = fs.readFileSync(input, "utf-8")
+  const lines = text
     .split(/\r?\n/)
-    .filter(line => line.trim() !== "")
-    .map(line => line.includes("/") ? line.trim() : line.trim() + "/32");
+    .filter((line) => line.trim() !== "")
+    .map((line) => (line.includes("/") ? line.trim() : line.trim() + "/32"));
 
-// формируем JSON
-const data = {
+  const data = {
     version: 1,
-    rules: [
-        {
-            ip_cidr: lines
-        }
-    ]
-};
+    rules: [{ ip_cidr: lines }],
+  };
 
-fs.writeFileSync(jsonFile, JSON.stringify(data, null, 2));
+  fs.writeFileSync("ipwhitelist.json", JSON.stringify(data, null, 2));
 
-console.log("JSON created:", jsonFile);
-
-// компиляция в srs
-try {
-    execSync(`sing-box.exe rule-set compile ${jsonFile} -o ${output}`, { stdio: "inherit" });
-    console.log("Done:", output);
-} catch (e) {
-    console.error("Error running sing-box");
-}
+  execSync(
+    `sing-box.exe rule-set compile ipwhitelist.json -o ipwhitelist.srs`,
+    {
+      stdio: "inherit",
+    }
+  );
+})();
