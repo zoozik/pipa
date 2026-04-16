5212a15b3a4a8ab7bbbe0a12fb37a5590830655d
epizod <>
2026-04-16 20:25:17 +0300

    x

--- convert.js
+++ convert.js
@@ -1,24 +1,30 @@
 const fs = require("fs");
 const { execSync } = require("child_process");
 
-const url =
-  "https://github.com/hxehex/russia-mobile-internet-whitelist/raw/refs/heads/main/ipwhitelist.txt";
+const cidrwhitelist =
+  "https://github.com/hxehex/russia-mobile-internet-whitelist/raw/refs/heads/main/cidrwhitelist.txt";
+
+const whitelist =
+  "https://github.com/hxehex/russia-mobile-internet-whitelist/raw/refs/heads/main/whitelist.txt";
 
 (async () => {
-  const res = await fetch(url);
-  const text = await res.text();
+  const cidrwhitelistRes = await fetch(cidrwhitelist);
+  const cidrwhitelistText = await cidrwhitelistRes.text();
 
-  const lines = text
+  const cidrwhitelistLines = cidrwhitelistText
     .split(/\r?\n/)
     .filter((line) => line.trim() !== "")
     .map((line) => (line.includes("/") ? line.trim() : line.trim() + "/32"));
 
-  const data = {
+  const cidrwhitelistData = {
     version: 3,
-    rules: [{ ip_cidr: lines }],
+    rules: [{ ip_cidr: cidrwhitelistLines }],
   };
 
-  fs.writeFileSync("ipwhitelist.json", JSON.stringify(data, null, 2));
+  fs.writeFileSync(
+    "ipwhitelist.json",
+    JSON.stringify(cidrwhitelistData, null, 2)
+  );
 
   execSync(
     `sing-box.exe rule-set compile ipwhitelist.json -o ipwhitelist.srs`,
@@ -26,4 +32,23 @@ const url =
       stdio: "inherit",
     }
   );
+
+  const whitelistRes = await fetch(whitelist);
+  const whitelistText = await whitelistRes.text();
+
+  const whitelistLines = whitelistText
+    .split(/\r?\n/)
+    .filter((line) => line.trim() !== "")
+    .map((line) => (line.includes("/") ? line.trim() : line.trim() + "/32"));
+
+  const whitelistData = {
+    version: 3,
+    rules: [{ domain_suffix: whitelistLines }],
+  };
+
+  fs.writeFileSync("whitelist.json", JSON.stringify(whitelistData, null, 2));
+
+  execSync(`sing-box.exe rule-set compile whitelist.json -o whitelist.srs`, {
+    stdio: "inherit",
+  });
 })();
